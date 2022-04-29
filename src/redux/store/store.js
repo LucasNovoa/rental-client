92a00c92d033eeb2be187a005bf952393d3d377a
tlleonart <tomas.lleonart@gmail.com>
2022-04-29 12:44:06 -0300

    Frontend - SlideBar (API data - no redux), Slice hotels, React-Icons lib

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import userReducer from '../slices/userSlice'
+import hotelReducer from '../slices/hotelSlice'
 
 export const store = configureStore({
   reducer: {
-    user: userReducer
+    user: userReducer,
+    hotel: hotelReducer
   }
 })
