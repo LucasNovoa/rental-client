5f3a74b11f95796c5fcafc03102cc8512916d79c
Facundo Casabella <facundocasabella@gmail.com>
2022-05-02 02:07:41 -0300

    Register page, link Home to Pages

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,32 +1,33 @@
-import React from 'react';
+import React from 'react'
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import { ProviderAuth } from '../hooks/useAuth';
+import { ProviderAuth } from '../hooks/useAuth'
 
-import { Layout } from '../containers/Layout';
+import { Layout } from '../containers/Layout'
 
-import { Home } from '../pages/Home';
-import { Profile } from '../pages/Profile';
-import { Login } from '../pages/Login';
+import { Home } from '../pages/Home'
+import { Profile } from '../pages/Profile'
+import { Login } from '../pages/Login'
+import Register from '../pages/Register'
 
-function App() {
+function App () {
   return (
     <ProviderAuth>
-      <div className="App">
+      <div className='App'>
         <BrowserRouter>
           <Layout>
             <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/profile" element={<Profile />} />
-              <Route path="/login" element={<Login />} />
-              {/* <Route path="/register" element={<Register />} /> */}
+              <Route path='/' element={<Home />} />
+              <Route path='/profile' element={<Profile />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Register />} />
             </Routes>
           </Layout>
         </BrowserRouter>
       </div>
     </ProviderAuth>
-  );
+  )
 }
 
-export { App };
+export { App }
