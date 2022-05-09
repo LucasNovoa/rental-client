bbccf489ae200f224fb0de521f83d8e647b17ee9
tlleonart <tomas.lleonart@gmail.com>
2022-05-09 10:37:01 -0300

    Frontend: estilos /hotel corregidos, agregado mapa de hotel

--- src/pages/Hotel.jsx
+++ src/pages/Hotel.jsx
@@ -6,15 +6,17 @@ import Header from '../components/Header/Header'
 import Amenities from '../components/Amenities/Amenities'
 import HotelCard from '../components/HotelCard/HotelCard'
 import { selectHotelByName } from '../redux/slices/hotelSlice'
+import Map from '../components/Map/Map'
 
 const Hotel = () => {
   const { name } = useParams()
   const hotel = useSelector((state) => selectHotelByName(state, name))
-
+  console.log(hotel)
   return (
     <div className='hotel'>
       <Header />
       <HotelCard hotel={hotel} />
+      <Map width='80vw' height={400} hotels={[hotel]} />
       <Amenities />
       <Footer />
     </div>
