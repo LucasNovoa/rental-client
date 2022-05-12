e113f758334abbd5a3b305c8a9e2904a3c0abb09
Nicolas Cohen <nicolascohen24@gmail.com>
2022-05-12 20:29:56 -0300

    GoogleAuth

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -25,10 +25,6 @@ function Login () {
     })
   }
 
-  const googleLogin = () => {
-    window.open('https://rental-app-server.herokuapp.com/api/v1/auth/google', '_self')
-  }
-
   return (
     <>
       <Header />
@@ -49,7 +45,7 @@ function Login () {
 
       </form>
 
-      <button className='btn' type='submit' onClick={googleLogin}>Ingresar con Google</button>
+      <button className='btn' type='submit' onClick={() => auth.signInGoogle()}>Ingresar con Google</button>
 
     </>
   )
