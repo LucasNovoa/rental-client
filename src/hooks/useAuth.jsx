ccc276aeee5f7a49faef3aeb7fe71bf89f89d098
Nicolas Cohen <nicolascohen24@gmail.com>
2022-05-13 15:54:37 -0300

    GG WP GOOGLE

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -62,7 +62,7 @@ function useProvideAuth () {
       }
     }
 
-    await axios.post('https://rental-app-server.herokuapp.com/api/v1/auth/change-password', { token, newPassword }, options)
+    await axios.post('http://localhost:3000/api/v1/auth/change-password', { token, newPassword }, options)
   }
 
   const logout = () => {
@@ -74,7 +74,31 @@ function useProvideAuth () {
   }
 
   const signInGoogle = async () => {
-    window.open('https://rental-app-server.herokuapp.com/api/v1/auth/google', '_self')
+    window.open('http://localhost:3001/api/v1/auth/google', '_self')
+
+    const options = {
+      headers: {
+        accept: '*/*',
+        'Content-Type': 'application/json'
+      }
+    }
+
+    const { data: access_token } = await axios.get('http://localhost:3001/api/v1/auth/getGoogleUser', options)
+
+    if (access_token) {
+      const token = access_token.token
+      const userId = access_token.user.id
+
+      Cookie.set('token', token, { expires: 5 })
+
+      axios.defaults.headers.Authorization = `Bearer ${token}`
+
+      const { data: user } = await axios.get(`http://localhost:3001/api/v1/users/${userId}`)
+
+      setUser(user)
+
+      window.localStorage.setItem('user', JSON.stringify(user))
+    }
   }
 
   return {
