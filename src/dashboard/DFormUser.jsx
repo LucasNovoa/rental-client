5cbc9e672b87a6ec9fc6cd1224fc59fdb9cd5ca3
Nicolas Cohen <nicolascohen24@gmail.com>
2022-05-17 05:38:34 -0300

    checkbox bye

--- src/dashboard/DFormUser.jsx
+++ src/dashboard/DFormUser.jsx
@@ -1,9 +1,13 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
-import { addUser } from './services/usersAPI'
+import { useNavigate } from 'react-router-dom'
+
+import { addUser, updateUser } from './services/usersAPI'
 
 export default function DFormUser ({ setOpen, setAlert, user }) {
   const formRef = useRef(null)
+
+  const navigate = useNavigate()
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -11,35 +15,39 @@ export default function DFormUser ({ setOpen, setAlert, user }) {
     const data = {
       role: formData.get('role'),
       typePerson: formData.get('typePerson'),
-      firstName: formData.get('firstName'),
-      lastName: formData.get('lastName'),
+      firstName: formData.get('firstName') === 'No Definido...' ? null : formData.get('firstName'),
+      lastName: formData.get('lastName') === 'No Definido...' ? null : formData.get('lastName'),
       image: formData.get('image'),
-      organization: formData.get('organization'),
+      organization: formData.get('organization') === 'No Definido...' ? null : formData.get('organization'),
       email: formData.get('email'),
       password: formData.get('password'),
       repeatPass: formData.get('repeatPass'),
-      isBanned: formData.get('isBanned'),
-      isSuscribed: formData.get('isSuscribed')
+      isBanned: formData.get('isBanned') === 'true',
+      isSuscribed: formData.get('isSuscribed') === 'true'
     }
 
-    console.log(data)
-
-    addUser(data).then(() => {
-      setAlert({
-        active: true,
-        message: 'Usuario agregado correctamente',
-        type: 'success',
-        autoClose: false
+    if (user) {
+      updateUser(user?.id, data).then(() => {
+        navigate('/dashboard/users')
       })
-      setOpen(false)
-    }).catch((error) => {
-      setAlert({
-        active: true,
-        message: error.message,
-        type: 'error',
-        autoClose: false
+    } else {
+      addUser(data).then(() => {
+        setAlert({
+          active: true,
+          message: 'Usuario agregado correctamente',
+          type: 'success',
+          autoClose: false
+        })
+        setOpen(false)
+      }).catch((error) => {
+        setAlert({
+          active: true,
+          message: error.message,
+          type: 'error',
+          autoClose: false
+        })
       })
-    })
+    }
   }
 
   return (
@@ -57,9 +65,9 @@ export default function DFormUser ({ setOpen, setAlert, user }) {
                 autoComplete='role-name'
                 className='mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-red-600 focus:border-red-600 sm:text-sm'
               >
-                <option value='admin'>Administrador</option>
-                <option value='owner'>Propietario</option>
-                <option value='customer'>Cliente</option>
+                <option selected={user?.role === 'admin'} value='admin'>Administrador</option>
+                <option selected={user?.role === 'owner'} value='owner'>Propietario</option>
+                <option selected={user?.role === 'customer'} value='customer'>Cliente</option>
               </select>
             </div>
             <div className='col-span-6 sm:col-span-3'>
@@ -72,69 +80,79 @@ export default function DFormUser ({ setOpen, setAlert, user }) {
                 autoComplete='typePerson-name'
                 className='mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-red-600 focus:border-red-600 sm:text-sm'
               >
-                <option value='natural'>Natural</option>
-                <option value='legal'>Legal</option>
+                <option selected={user?.typePerson === 'natural'} value='natural'>Natural</option>
+                <option selected={user?.typePerson === 'legal'} value='legal'>Legal</option>
               </select>
             </div>
             <div className='col-span-6 sm:col-span-3'>
               <label htmlFor='firstName' className='block text-sm font-medium text-gray-700'>
                 First Name
               </label>
-              <input type='text' name='firstName' id='firstName' className='mt-1 focus:ring-red-600 focus:border-red-600 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md' />
+              <input type='text' defaultValue={user?.firstName === null ? 'No Definido...' : user?.firstName} name='firstName' id='firstName' className='mt-1 focus:ring-red-600 focus:border-red-600 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md' />
             </div>
             <div className='col-span-6 sm:col-span-3'>
               <label htmlFor='lastName' className='block text-sm font-medium text-gray-700'>
                 Last Name
               </label>
-              <input type='text' name='lastName' id='lastName' className='mt-1 focus:ring-red-600 focus:border-red-600 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md' />
+              <input type='text' defaultValue={user?.lastName === null ? 'No Definido...' : user?.lastName} name='lastName' id='lastName' className='mt-1 focus:ring-red-600 focus:border-red-600 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md' />
             </div>
             <div className='col-span-6'>
               <label htmlFor='image' className='block text-sm font-medium text-gray-700'>
                 Imagen de Perfil
               </label>
-              <input type='text' name='image' id='image' className='mt-1 focus:ring-red-600 focus:border-red-600 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md' />
+              <input type='text' defaultValue={user?.image} name='image' id='image' className='mt-1 focus:ring-red-600 focus:border-red-600 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md' />
             </div>
             <div className='col-span-6'>
               <label htmlFor='organization' className='block text-sm font-medium text-gray-700'>
                 Organización
               </label>
-              <input type='text' name='organization' id='organization' className='mt-1 focus:ring-red-600 focus:border-red-600 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md' />
+              <input type='text' defaultValue={user?.organization === null ? 'No Definido...' : user?.organization} name='organization' id='organization' className='mt-1 focus:ring-red-600 focus:border-red-600 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md' />
             </div>
             <div className='col-span-6'>
               <label htmlFor='email' className='block text-sm font-medium text-gray-700'>
                 Email
               </label>
-              <input type='email' name='email' id='email' className='mt-1 focus:ring-red-600 focus:border-red-600 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md' />
+              <input type='email' name='email' defaultValue={user?.email} id='email' className='mt-1 focus:ring-red-600 focus:border-red-600 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md' />
             </div>
             <div className='col-span-6 sm:col-span-3'>
               <label htmlFor='password' className='block text-sm font-medium text-gray-700'>
                 Contraseña
               </label>
-              <input type='password' name='password' id='password' className='mt-1 focus:ring-red-600 focus:border-red-600 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md' />
+              <input type='password' name='password' defaultValue={user?.password} id='password' className='mt-1 focus:ring-red-600 focus:border-red-600 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md' />
             </div>
             <div className='col-span-6 sm:col-span-3'>
               <label htmlFor='repeatPass' className='block text-sm font-medium text-gray-700'>
                 Repetir contraseña
               </label>
-              <input type='password' name='repeatPass' id='repeatPass' className='mt-1 focus:ring-red-600 focus:border-red-600 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md' />
+              <input type='password' name='repeatPass' defaultValue={user?.repeatPass} id='repeatPass' className='mt-1 focus:ring-red-600 focus:border-red-600 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md' />
             </div>
-            <div className='col-span-6'>
-              <label htmlFor='isBanned' className='inline-flex items-center'>
-                <input
-                  defaultChecked={user?.isBanned}
-                  type='checkbox' name='isBanned' id='isBanned' className='rounded bg-gray-200  border-transparent focus:border-transparent focus:bg-gray-200 text-gray-700 focus:ring-1 focus:ring-offset-2 focus:ring-gray-500'
-                />
-                <span className='ml-2'>Penalizado</span>
+            <div className='col-span-6 sm:col-span-3'>
+              <label htmlFor='isBanned' className='block text-sm font-medium text-gray-700'>
+                Penalizado
               </label>
+              <select
+                id='isBanned'
+                name='isBanned'
+                autoComplete='isBanned-name'
+                className='mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-red-600 focus:border-red-600 sm:text-sm'
+              >
+                <option selected={user?.isBanned === true} value='true'>Verdadero</option>
+                <option selected={user?.isBanned === false} value='false'>Falso</option>
+              </select>
             </div>
-            <div className='col-span-6'>
-              <label htmlFor='isSuscribed' className='inline-flex items-center'>
-                <input
-                  defaultChecked={user?.isSuscribed}
-                  type='checkbox' name='isSuscribed' id='isSuscribed' className='rounded bg-gray-200  border-transparent focus:border-transparent focus:bg-gray-200 text-gray-700 focus:ring-1 focus:ring-offset-2 focus:ring-gray-500'
-                />
-                <span className='ml-2'>Subscriptor</span>
+            <div className='col-span-6 sm:col-span-3'>
+              <label htmlFor='isSuscribed' className='block text-sm font-medium text-gray-700'>
+                Subscriptor
               </label>
+              <select
+                id='isSuscribed'
+                name='isSuscribed'
+                autoComplete='isSuscribed-name'
+                className='mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-red-600 focus:border-red-600 sm:text-sm'
+              >
+                <option selected={user?.isSuscribed === true} value='true'>Verdadero</option>
+                <option selected={user?.isSuscribed === false} value='false'>Falso</option>
+              </select>
             </div>
           </div>
         </div>
