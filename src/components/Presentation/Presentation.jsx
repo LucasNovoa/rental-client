e1a7becf4687dce5448432c5e2cf1fd18c294994
Ernesto Abril <abrilernesto@gmail.com>
2022-05-19 09:28:03 -0500

    Cambie texto en componente Presentation

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './presentation.scss'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./presentation.scss";
 
-function Presentation () {
+function Presentation() {
   return (
-    <section className='presentation'>
-      <div className='presentation__data'>
-        <h1 className='presentation__data__title'>Encuentra tu alojamiento ideal</h1>
-        <p className='presentation__data__description'>Non consectetur a erat nam at lectus urna duis convallis molestie nunc non blandit massa ut etiam sit amet nisl purus in mollis nunc sed et magnis dis parturient.</p>
-        <div className='presentation__data__buttonRow'>
-          <Link to='/hotels'>
-            <button className='presentation__data__buttonRow__red'>Explorar Alojamientos</button>
+    <section className="presentation">
+      <div className="presentation__data">
+        <h1 className="presentation__data__title">
+          Encuentra tu alojamiento ideal
+        </h1>
+        <p className="presentation__data__description">
+          Hoy más que nunca, las personas tienen más flexibilidad para decidir,
+          cuando y a donde viajan. Te ofrecemos Rental app, la mejor plataforma
+          para buscar y reservar hospedaje y servicios
+        </p>
+        <div className="presentation__data__buttonRow">
+          <Link to="/hotels">
+            <button className="presentation__data__buttonRow__red">
+              Explorar Alojamientos
+            </button>
           </Link>
-          <Link to='/about'>
-            <button className='presentation__data__buttonRow__white'>Sobre nosotros</button>
+          <Link to="/about">
+            <button className="presentation__data__buttonRow__white">
+              Sobre nosotros
+            </button>
           </Link>
         </div>
       </div>
     </section>
-  )
+  );
 }
 
-export default Presentation
+export default Presentation;
